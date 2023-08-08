2217974e1f9cd4ce358a4b8c6f4b50976dedfc6f
Misiac <MisiacYT@gmail.com>
2023-08-08 11:55:38 +0200

    basic circle drawing

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -6,17 +6,20 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 
 public class GameScreen implements Screen {
 
     Game game;
     Sound sound;
+    private int circleX = 240;
+    private int circleY = 240;
 
 
     public GameScreen(Game game) {
         this.game = game;
         sound = Gdx.audio.newSound(Gdx.files.internal("song.mp3"));
-        sound.play();
+//        sound.play();
     }
 
     @Override
@@ -33,6 +36,18 @@ public class GameScreen implements Screen {
         Cursor cursor = Gdx.graphics.newCursor(pm, 64, 64);
         pm.dispose();
         Gdx.graphics.setCursor(cursor);
+
+        Texture hitCircle = new Texture(Gdx.files.internal("hitcircle.png"));
+        Texture hitCircleOverlay = new Texture(Gdx.files.internal("hitcircleoverlay.png"));
+        Texture approachCircle = new Texture(Gdx.files.internal("approachcircle.png"));
+        Texture circleNumber = new Texture(Gdx.files.internal("numbers/default-0.png"));
+
+        game.batch.begin();
+        game.batch.draw(hitCircle, circleX, circleY);
+        game.batch.draw(hitCircleOverlay, circleX, circleY);
+        game.batch.draw(approachCircle, circleX, circleY);
+        game.batch.draw(circleNumber, circleX+39, circleY+39);   // 128:2 - 25/2
+        game.batch.end();
 
     }
 
